47bf1f10b9aab6d72afef2d3d82349e97b84240a
Fepitt <>
2017-04-22 02:27:01 -0300

    Corrigido parcialmente o erro 'NullPointer' causado pelo filtro 'AuthLoginFilter'; 'FrontController' o Servlet foi retirado do pacote Controller e posto no Web;

--- allgraph/src/java/br/mack/gs/web/FrontController.java
+++ allgraph/src/java/br/mack/gs/web/FrontController.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.mack.gs.controller;
+package br.mack.gs.web;
 
+import br.mack.gs.controller.Controller;
+import br.mack.gs.controller.ControllerFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
