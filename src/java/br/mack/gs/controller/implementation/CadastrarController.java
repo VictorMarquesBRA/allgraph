0dda6d17bf60b9ae681ec10079d8346f3c68d71a
enrydell <henriquedell1@gmail.com>
2017-05-10 19:57:35 -0300

    Controller do Cadastro terminado

--- src/java/br/mack/gs/controller/implementation/CadastrarController.java
+++ src/java/br/mack/gs/controller/implementation/CadastrarController.java
@@ -7,6 +7,12 @@ package br.mack.gs.controller.implementation;
 
 import br.mack.gs.controller.AbstractController;
 import br.mack.gs.entity.UserAllGraph;
+import br.mack.gs.persistence.UserAllGraphDAO;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
@@ -14,6 +20,8 @@ import br.mack.gs.entity.UserAllGraph;
  */
 public class CadastrarController extends AbstractController {
 
+    UserAllGraphDAO userAllGraphDAO = lookupUserAllGraphDAOBean();
+    
     @Override
     public void execute() {
         String username = this.getRequest().getParameter("username"),
@@ -29,12 +37,31 @@ public class CadastrarController extends AbstractController {
             userag.setEmail(email);
             userag.setPassword(password);
             
-            // Código para checar se usuário já está cadastrado
-            
-            this.getRequest().getSession().setAttribute("user", userag);
-            this.setResponsePage(this.getRequest().getContextPath() + "/user/feed.jsp");
+            UserAllGraph userExistente = userAllGraphDAO.getByEmail(email);
+            if(userExistente == null) {
+                userAllGraphDAO.inserir(userag);
+                UserAllGraph usuarioInserido = userAllGraphDAO.getByEmail(email);
+                this.getRequest().getSession().setAttribute("user", usuarioInserido);
+                this.setResponsePage(this.getRequest().getContextPath() + "/user/feed.jsp");                
+            } else {
+                this.getRequest().getSession().setAttribute("error", "Usuário já cadastrado");
+                this.setResponsePage(this.getRequest().getContextPath() + "index.jsp");
+            }            
+        } else {
+            this.getRequest().getSession().setAttribute("error", "Há campos em branco ou senhas não são iguais");
+            this.setResponsePage(this.getRequest().getContextPath() + "index.jsp");
         }
         
+    }
+
+    private UserAllGraphDAO lookupUserAllGraphDAOBean() {
+        try {
+            Context c = new InitialContext();
+            return (UserAllGraphDAO) c.lookup("java:global/allgraph/UserAllGraphDAO!br.mack.gs.persistence.UserAllGraphDAO");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
     }
     
     
--- web/index.jsp
+++ web/index.jsp
@@ -15,6 +15,14 @@
         <input name="password" type="password" placeholder="Senha" /> 
         <input name="password_to_verify" type="password" placeholder="Confirmar senha" /> 
         <input name="cmd" type="hidden" value="Cadastrar" />
+        <c:choose>
+            <c:when test="${error == 'Usuário já cadastrado'}">
+                <span class="error">Usuário já cadastrado</span>
+            </c:when>
+            <c:when test="${error == 'Há campos em branco ou senhas não são iguais'}">
+                <span class="error">Há campos em branco ou senhas não são iguais</span>
+            </c:when>
+        </c:choose>
         <button type="submit">Cadastrar</button>
     </form>
     <form id="formularioLogin" action="FrontController" method="POST">
